7d9121ad471cf984b1fdbbbe7e2864a8ea69918a
rupertoss <rakoski.rupertoss@gmail.com>
2018-01-08 23:03:45 +0100

    ver 03
    model annotations for db

--- src/main/java/com/rupertoss/checkout/model/Cart.java
+++ src/main/java/com/rupertoss/checkout/model/Cart.java
@@ -3,26 +3,35 @@ package com.rupertoss.checkout.model;
 import java.util.Map;
 import java.util.TreeMap;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapKeyColumn;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "carts")
+@Table(name = "Cart")
 public class Cart {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	public long id;
 	
 	@NotNull
 	@ElementCollection
+	@CollectionTable(name="Cart_Items", joinColumns = @JoinColumn(name = "ID"))
+	@MapKeyColumn(name = "ItemID")
+	@Column(name = "ItemQuantity")
 	public Map<Integer, Integer> items = new TreeMap<Integer, Integer>();
 	
+	@Column(name = "CartValue")
 	public double value;
 
 	public long getId() {
@@ -48,6 +57,4 @@ public class Cart {
 	public void setValue(double value) {
 		this.value = value;
 	}
-
-	
 }
--- src/main/java/com/rupertoss/checkout/model/Item.java
+++ src/main/java/com/rupertoss/checkout/model/Item.java
@@ -1,5 +1,6 @@
 package com.rupertoss.checkout.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,24 +11,29 @@ import javax.persistence.Table;
 import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
-@Table(name = "items")
+@Table(name = "Item")
 public class Item {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	public int id;
 	
 	@NotBlank
+	@Column(name = "Price")
 	public double price;
 	
 	@NotBlank
 	@Lob
+	@Column(name = "Description")
 	public String description;
 	
 	@NotBlank
+	@Column(name = "SpecialQuantity")
 	public int specialQuantity;
 	
 	@NotBlank
+	@Column(name = "SpecialPrice")
 	public double specialPrice;
 
 	public int getId() {
